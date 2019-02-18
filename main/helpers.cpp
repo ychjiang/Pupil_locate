6f434a7d7bc108a28006ca13f6d9c52b6034e208
ychjiang <ychjiang33@163.com>
2019-02-18 10:53:35 +0800

    first commit

---
+++ main/helpers.cpp
@@ -0,0 +1,44 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+
+#include "constants.h"
+
+using namespace std;
+using namespace cv;
+
+bool rectInImage(cv::Rect rect, cv::Mat image) {
+  return rect.x > 0 && rect.y > 0 && rect.x+rect.width < image.cols &&
+  rect.y+rect.height < image.rows;
+}
+
+bool inMat(cv::Point p,int rows,int cols) {
+  return p.x >= 0 && p.x < cols && p.y >= 0 && p.y < rows;
+}
+
+cv::Mat matrixMagnitude(const cv::Mat &matX, const cv::Mat &matY) {
+  cv::Mat mags(matX.rows,matX.cols,CV_64F);
+  for (int y = 0; y < matX.rows; ++y) {
+    const double *Xr = matX.ptr<double>(y), *Yr = matY.ptr<double>(y);
+    double *Mr = mags.ptr<double>(y);
+    for (int x = 0; x < matX.cols; ++x) {
+      double gX = Xr[x], gY = Yr[x];
+      double magnitude = sqrt((gX * gX) + (gY * gY));
+      Mr[x] = magnitude;
+    }
+  }
+  return mags;
+}
+
+double computeDynamicThreshold(const cv::Mat &mat, double stdDevFactor) {
+  cv::Scalar stdMagnGrad, meanMagnGrad;
+  cv::meanStdDev(mat, meanMagnGrad, stdMagnGrad);
+  float zhangxu_test =  sqrt((float)(mat.rows*mat.cols));
+  double stdDev = stdMagnGrad[0] / zhangxu_test;
+  return stdDevFactor * stdDev + meanMagnGrad[0];
+}
